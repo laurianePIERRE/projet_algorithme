f985bc5eb516d74d531eca90cafba092a30a5c1e
laurianePIERRE <pierrelauriane08@gmail.com>
2024-04-09 10:30:40 +0200

    first commit

--- hull_convex.py
+++ hull_convex.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def generate_fich_points():
     """
@@ -21,8 +22,41 @@ def list_coordonnes(file) :
             cordonnees.append((x,y))
     return cordonnees
 
+def coordonnee_gauche(coordonnees) :
+    # on initialise
+    coordonnees_gauche = coordonnees[0]
+    for coord in coordonnees:
+        if coord[0] < coordonnees_gauche[0] :
+            coordonnees_gauche=coord
+    return coordonnees_gauche
 
-
+def calcul_angle(A,B,pivot) :
+    APivot=(pivot[0]-A[0],pivot[1]-A[1])
+    PivotB = (B[0]-pivot[0],B[1]-pivot[1])
+    print (APivot)
+    print(PivotB)
+    scal = APivot[0]*PivotB[0] +APivot[1]*PivotB[1]
+    print (scal)
+    inter= ((pivot[0]-A[0])**2,(pivot[1]-A[1])**2)
+    distanceXPivot=math.sqrt((pivot[0]-A[0])**2+(pivot[1]-A[1])**2)
+    print(inter)
+    print (distanceXPivot)
+    inter2 = ((pivot[0]-B[0])**2, (pivot[1]-B[1])**2)
+    print (inter2)
+    distanceYPivot = math.sqrt((pivot[0]-B[0])**2 + (pivot[1]-B[1])**2)
+    print(distanceYPivot)
+    cosangle = scal/(distanceXPivot*distanceYPivot)
+    print(cosangle)
+    angleXPivotY = math.acos(scal/(distanceXPivot*distanceYPivot))
+    angle_degres = angleXPivotY*180/math.pi
+    return angle_degres
 
 #generate_fich_points()
-list_coordonnes("points_random.txt")
+coordonnees = list_coordonnes("points_random.txt")
+print(coordonnees)
+coordonnee_initiale = coordonnee_gauche(coordonnees)
+print (coordonnee_initiale)
+print(coordonnees[0])
+print(coordonnees[1])
+print(coordonnee_initiale)
+print(calcul_angle(coordonnees[0],coordonnees[1],coordonnee_initiale))
